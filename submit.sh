813980c1e48591348f52d7ed9e91f7e220546efd
Peter Harmer <pharmer@wisc.edu>
2023-04-07 18:32:47 -0500

    Lunchbox data download, currently setup for only 1 year in submit

---
+++ submit.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+rm -rf out; mkdir out # Write desired output (by redirecting stdout) here.
+rm -rf slurm_out; mkdir slurm_out # Let Slurm write stdout & stderr here.
+
+jobId1=$(sbatch --array=1\
+                --output="slurm_out/slurm-%A_%a.out" \
+                --error="slurm_out/slurm-%A_%a.err" \
+                ./getData.sh)
+
+jobId1=$(echo $jobId1 | sed 's/Submitted batch job //')
