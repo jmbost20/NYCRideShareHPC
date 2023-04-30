5928e00c139121bbaac7016dbba14e9e127300bf
Peter Harmer <pharmer@wisc.edu>
2023-04-30 11:43:48 -0500

    Updated submit.sh to run packageSetup

--- projectHPC/submit.sh
+++ projectHPC/submit.sh
@@ -10,3 +10,15 @@ jobId1=$(sbatch --array=1-4\
                 ./getData.sh)
 
 jobId1=$(echo $jobId1 | sed 's/Submitted batch job //')
+
+jobId2=$(sbatch --output="slurm_out/slurm-%A_%a.out" \
+                 --error="slurm_out/slurm-%A_%a.err" \
+		 --dependency=afterok:$jobId1 \
+                ./packageSetup.sh)
+
+jobId2=$(echo $jobId2 | sed 's/Submitted batch job //')
+
+jobId3=$(sbatch --output="slurm_out/slurm-%A_%a.out" \
+                 --error="slurm_out/slurm-%A_%a.err" \
+                 --dependency=afterok:$jobId2 \
+                ./packageSetup.sh)
