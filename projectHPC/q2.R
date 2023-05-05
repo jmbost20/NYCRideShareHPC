aac3d7c6c80040bed8ae14ddee46e20cd8e69c8d
Abhimanyu Dev Gupta <42531250+AbhimanyuGupta20@users.noreply.github.com>
2023-05-05 11:11:32 -0500

    Update q2.R

--- projectHPC/q2.R
+++ projectHPC/q2.R
@@ -1,39 +1,51 @@
 #!/usr/bin/Rscript
+library(arrow)
 
-data <- read.csv("dummydata.csv",header = FALSE)
-head(data)
-#data$V10[0]
+df4 <- read_parquet("~/Downloads/Parquets/fhvhv_tripdata_2020-04.parquet")
+df5 <- read_parquet("~/Downloads/Parquets/fhvhv_tripdata_2020-05.parquet")
+df6 <- read_parquet("~/Downloads/Parquets/fhvhv_tripdata_2020-06.parquet")
+df7 <- read_parquet("~/Downloads/Parquets/fhvhv_tripdata_2020-07.parquet")
+df8 <- read_parquet("~/Downloads/Parquets/fhvhv_tripdata_2020-08.parquet")
+df9 <- read_parquet("~/Downloads/Parquets/fhvhv_tripdata_2020-09.parquet")
+df10 <- read_parquet("~/Downloads/Parquets/fhvhv_tripdata_2020-10.parquet")
+
+df4sub <- df4[1:1000, ]
+df5sub <- df5[1:1000, ]
+df6sub <- df6[1:1000, ]
+df7sub <- df7[1:1000, ]
+df8sub <- df8[1:1000, ]
+df9sub <- df9[1:1000, ]
+df10sub <- df10[1:1000, ]
+
+combined <- rbind(df4sub, df5sub, df6sub, df7sub,df8sub, df9sub, df10sub)
 
 miles <- c()
 driverpay <- c()
-miles <- data$V10
-driverpay <- data$V19
-
-
+miles <- combined["trip_miles"]
+driverpay <- combined["driver_pay"]
 
 dates <- c()
 profit <- c()
-datalen <- nrow(data)
+datalen <- nrow(combined)
 
-for (i in 1:datalen) {
-  dates[i] <- data$V4[i]
-}
+dates <- combined$request_datetime
 
 miles <- c()
 driverpay <- c()
-miles <- data$V10
-miles <- miles[2:length(miles)]
-driverpay <- data$V19
-driverpay <- driverpay[2:length(driverpay)]
+miles <- combined$"trip_miles"
 
-for (i in 1:datalen-1) {
-  profit[i] <- as.double(driverpay[i]) / as.double(miles[i]) #per mile profit
+driverpay <- combined$"driver_pay"
+
+for (i in 1:datalen) {
+  profit[i] <- as.double(driverpay[i]) / as.double(miles[i])
 }
 
-dates <- dates[2:length(dates)]
+length(dates)
+length(profit)
 
 dates <- substr(dates, 6, 7)
 
+profit[is.infinite(profit)] <- 0
 
 distribution <- tapply(profit, dates, mean)
 distribution
